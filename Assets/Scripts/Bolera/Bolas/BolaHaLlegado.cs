387210f458dff664c42b30630ceca2cf16cf8854
JorgePastorCastilla <jordi131313@gmail.com>
2025-03-19 13:04:53 +0100

    Cambiados los prebas de las bolas y modificado el script para que se pasen los turnos cuando hagas tiradas

--- Assets/Scripts/Bolera/Bolas/BolaHaLlegado.cs
+++ Assets/Scripts/Bolera/Bolas/BolaHaLlegado.cs
@@ -7,23 +7,36 @@ public class BolaHaLlegado : MonoBehaviour
     public double contador;
     public double tiempoLimite = 10;
     public static UnityEvent pasarTurno = new UnityEvent();
-
+    
+    public bool haTocadoPista = false;
     // Update is called once per frame
     void Update()
     {
-        contador += Time.deltaTime;
-        if (contador >= tiempoLimite)
+        if (haTocadoPista)
         {
-            //Llamar para cambiar de turno
-            Debug.Log("Se acabo el tiempo, cambio de turno");
-            pasarTurno.Invoke();
-            contador = 0;
+            contador += Time.deltaTime;
+            if (contador >= tiempoLimite)
+            {
+                //Llamar para cambiar de turno
+                Debug.Log("Se acabo el tiempo, cambio de turno");
+                pasarTurno.Invoke();
+                contador = 0;
+                haTocadoPista = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("PistaBolos"))
+        {
+            haTocadoPista = true;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("BowlingBall"))
+        if (other.gameObject.CompareTag("FinalPistaBolos"))
         {
             Debug.Log("Bola ha llegdo, cambio de turno");
             pasarTurno.Invoke();
