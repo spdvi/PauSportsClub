78f9f2d709af81202ac777290ab22c8e75f49b16
spdvi <gjm@paucasesnovescifp.cat>
2025-03-26 12:27:51 +0100

    Move object with RightThumbstickInputAction

---
+++ Assets/DevMikeShooter/Scripts/RightThumbstickInputAction.cs
@@ -0,0 +1,31 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RightThumbstickInputAction : MonoBehaviour
+{
+    public InputActionReference rightThumbstick;
+    public Transform moveObjectTransform;
+    public TextMeshProUGUI yAxisValueText;
+
+    private void OnEnable()
+    {
+        rightThumbstick.action.performed += ThumbstickActivated;
+    }
+
+    private void OnDisable()
+    {
+        rightThumbstick.action.performed -= ThumbstickActivated;
+    }
+
+
+    private void ThumbstickActivated(InputAction.CallbackContext ctx)
+    {
+        Vector2 rightThumbstickValue = ctx.ReadValue<Vector2>();
+        yAxisValueText.text = rightThumbstickValue.x.ToString() + ", " + rightThumbstickValue.y.ToString();
+        Vector3 moveDirection = new Vector3(rightThumbstickValue.x, 0, rightThumbstickValue.y);
+        moveObjectTransform.position += moveDirection;
+    }
+    
+}
--- Assets/DevMikeShooter/Scripts/XButtonPressInputAction.cs
+++ Assets/DevMikeShooter/Scripts/XButtonPressInputAction.cs
@@ -6,22 +6,35 @@ using UnityEngine.InputSystem;
 public class XButtonPressInputAction : MonoBehaviour
 {
     public InputActionReference xButton;
+    public InputActionReference yButton;
+    public InputActionReference rightThumbstick;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI yAxisValueText;
+    public Rigidbody rb;
     public int score = 0;
-    
+    public float yAxisValue = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
         xButton.action.started += UpdateScore;
         xButton.action.canceled += XButtonReleased;
+        yButton.action.performed += YButtonPressed;
     }
     
     private void OnDisable()
     {
         xButton.action.started -= UpdateScore;
         xButton.action.canceled -= XButtonReleased;
+        yButton.action.performed -= YButtonPressed;
     }
 
+    private void YButtonPressed(InputAction.CallbackContext ctx)
+    {
+        yAxisValue = ctx.ReadValue<float>();
+        yAxisValueText.text = "Y Axis: " + yAxisValue.ToString();
+        rb.AddForce(Vector3.up * yAxisValue * 200);
+    }
+    
     private void XButtonReleased(InputAction.CallbackContext context)
     {
         // ...
